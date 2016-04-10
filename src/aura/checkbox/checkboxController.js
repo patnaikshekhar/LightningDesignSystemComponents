a0658481ddf4543435517c9c1491c8fcf97c4626
Shekhar Patnaik <patnaikshekhar@gmail.com>
2016-04-10 20:23:20 +0100

    Fixed one way databinding for checkbox

--- src/aura/checkbox/checkboxController.js
+++ src/aura/checkbox/checkboxController.js
@@ -1,9 +1,8 @@
 ({
 	onChange : function(component, event, helper) {
         
-        var currentValue = component.get('v.value');
+        var currentValue = component.get('v.checked');
         var newValue = currentValue ? false : true;
-        component.set('v.value', newValue);
         component.getEvent('onChange').fire({ domEvent: event, value: newValue})
 	}
 })
