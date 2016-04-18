2ccf48ef7373db98189fc388ba7381d1c2db2f1b
Shekhar Patnaik <patnaikshekhar@gmail.com>
2016-04-18 16:06:10 +0100

    Menu - Added seperators

--- src/aura/menu/menuController.js
+++ src/aura/menu/menuController.js
@@ -1,6 +1,10 @@
 ({
+    init: function(component, event, helper) {
+    	helper.setup(component);    
+    },
+    
 	showMoreClicked : function(component, event, helper) {
-		var showMenu = component.get('v.showMenu');
-        component.set('v.showMenu', showMenu ? false : true);
+		var showMenu = component.get('v.show');
+        component.set('v.show', showMenu ? false : true);
 	}
 })
---
+++ src/aura/menuHeader/menuHeaderHelper.js
@@ -0,0 +1,27 @@
+({
+	setup : function(component) {
+		var body = component.get('v.body');
+        
+        // Filter out the sections
+        var filteredMenuItems = body.filter(function(c) {
+            return (c.getName() == 'ldsc$menuItem' || c.getName() == 'c$menuItem'
+                    || c.getName() == 'ldsc$menuSeperator' || c.getName() == 'c$menuSeperator');
+        });
+        
+        var menuItems = filteredMenuItems.reduceRight(function(acc, next) {
+            if (next.getName() == 'ldsc$menuSeperator' || next.getName() == 'c$menuSeperator') {
+                if (acc.length > 0) {
+                	var previous = acc[0];
+                	previous.set('v.class', 'slds-has-divider--top-space');
+                    return acc;
+                } else {
+                	return acc;    
+                }
+            } else {
+                return [next].concat(acc);
+            }
+        }, []);
+        
+        component.set('v.menuItems', menuItems);
+	}
+})
