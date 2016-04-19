a50056feeb649d1b28525f413e6bcf04969a9c1f
Shekhar Patnaik <patnaikshekhar@gmail.com>
2016-04-20 00:51:48 +0100

    Menu - Added auto generation capability

--- src/aura/menu/menuHelper.js
+++ src/aura/menu/menuHelper.js
@@ -1,6 +1,45 @@
 ({
-	setup : function(component) {
-		var body = component.get('v.body');
+	setup: function(component) {
+        
+        var helper = this;
+        
+        helper.createMenuItems(component, function() {
+        	helper.transformComponentsInBody(component);    
+        });
+	},
+    
+    createMenuItems: function(component, callback) {
+    	var menuItems = component.get('v.items');
+        
+        if (menuItems) {
+            
+            if (menuItems.length > 0) {
+            	var menuItemsComponents = menuItems.map(function(value) {
+                    return ['c:menuItem', {
+                        label: value
+                    }];
+                });
+
+                $A.createComponents(menuItemsComponents, function(components, status, err) {
+                    if (status === "SUCCESS") {
+                        var existingBody = component.get('v.body');
+                        component.set('v.body', existingBody.concat(components));
+                        callback();
+                    } else {
+                        console.err('Error has occured when creating components', err);
+                        callback();
+                    }
+                });	    
+            } else {
+                callback();
+            }
+        } else {
+            callback();
+        }
+	},
+    
+    transformComponentsInBody: function(component) {
+        var body = component.get('v.body');
         
         // Filter out the sections
         var filteredMenuItems = body.filter(function(c) {
@@ -27,5 +66,5 @@
         component.set('v.sections', body.filter(function(c) {
             return (c.getName() == 'ldsc$menuHeader' || c.getName() == 'c$menuHeader');
         }));
-	}
+    }
 })
